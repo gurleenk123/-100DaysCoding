437492abf036062768c67bee619b06857c058e32
gurleenk123 <56589123+gurleenk123@users.noreply.github.com>
2022-01-13 13:42:59 +0530

    Create MaximumUsingRecursion.java

---
+++ Day3/MaximumUsingRecursion.java
@@ -0,0 +1,23 @@
+public class MaximumUsingRecursion 
+{
+	public static int maxx(int arr[],int index)
+	{
+		if(index==arr.length-1)
+			return arr[index-1];
+		int ms=maxx(arr,index+1);
+		if(ms>arr[index])
+			return ms;
+		else
+			return arr[index];
+	}
+	
+
+	public static void main(String[] args) 
+	{
+		
+		int a[]= {12,23,11,3,20};
+		System.out.println("max ele is :"+maxx(a,0));
+
+	}
+
+}

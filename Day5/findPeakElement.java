ca6424f745d001f31dc8304a11216746355ba769
gurleenk123 <56589123+gurleenk123@users.noreply.github.com>
2022-01-15 18:01:51 +0530

    Create findPeakElement.java

---
+++ Day5/findPeakElement.java
@@ -0,0 +1,46 @@
+//Leetcode Q 852. Peak Index in a Mountain Array
+//A peak element is an element that is strictly greater than its neighbors.
+
+//Given an integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+public class PeakElement {
+	public static int findPeakElement(int[] nums) 
+    {
+		int n=nums.length;
+		if(nums.length == 1){
+	        return 0;
+	    }
+	    if(nums[0] > nums[1]){
+	       return 0; 
+	    }
+	    if(nums[n-1] > nums[n-2]){
+	        return n-1;
+	    }
+	    
+	    int l = 1,r = n-2;
+	    
+	    while(l <= r){
+	        
+	        int mid = l + (r-l)/2;
+	        if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+	            return mid;
+	        }
+	        else if(nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1] ){
+	            l = mid + 1;
+	        }
+	        else{
+	            r = mid - 1;
+	        }
+	    }
+	    return -1;
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {1,2,1,3,5,6,4};
+		System.out.println(findPeakElement(a));
+
+	}
+
+}
